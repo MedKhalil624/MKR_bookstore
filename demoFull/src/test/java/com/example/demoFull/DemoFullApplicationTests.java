9cd5f4eb0776f96eafd25ca4aa4951a3bfd4ee60
mkrahali <mkrahali@D-TN-9L8YTJ2.europe.corp.altran.com>
2020-11-17 17:42:24 +0100

    1st commit : Create spring boot app and configure the application properties

---
+++ demoFull/src/test/java/com/example/demoFull/DemoFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
